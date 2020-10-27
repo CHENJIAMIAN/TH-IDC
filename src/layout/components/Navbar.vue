785b7ee2cdb1ff8dfe8c608588487ba965757e2a
CJM <571574085@qq.com>
2020-10-27 10:02:49 +0800

    注释掉错误监控报告机制

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -8,17 +8,17 @@
     /> -->
 
     <div class="right-menu">
-      <template v-if="device !== 'mobile'">
+      <!-- <template v-if="device !== 'mobile'"> -->
         <!-- <search id="header-search" class="right-menu-item" /> -->
 
-        <error-log class="errLog-container right-menu-item hover-effect" />
+        <!-- <error-log class="errLog-container right-menu-item hover-effect" /> -->
 
         <!-- <screenfull id="screenfull" class="right-menu-item hover-effect" /> -->
 
         <!-- <el-tooltip content="Global Size" effect="dark" placement="bottom">
           <size-select id="size-select" class="right-menu-item hover-effect" />
         </el-tooltip> -->
-      </template>
+      <!-- </template> -->
 
       <div class="avatar-wrapper">
         <a title="消息"><img src="@/assets/img/xx.png"/></a>
